c68c138e0000f15ae06c422c07e9c63ea112c2eb
jamliddin123 <jamoliddinshodmonov89@gmail.com>
2024-08-13 19:28:15 +0500

    done!

--- online_shop/forms.py
+++ online_shop/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from online_shop.models import Comment, Order, Product
 
@@ -31,3 +32,36 @@ class ProductModelForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField(required=True)
     password = forms.CharField(required=True)
+
+
+class RegisterForm(forms.ModelForm):
+    confirm_password = forms.CharField(max_length=255)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def clean_username(self):
+        username = self.data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'This {username} is already exists')
+        return username
+
+    def clean_password(self):
+        password = self.data.get('password')
+        confirm_password = self.data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError('Passwords do not match')
+        return self.cleaned_data['password']
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        user.is_active = True
+        user.is_superuser = True
+        user.is_staff = True
+
+        if commit:
+            user.save()
+
+        return user
